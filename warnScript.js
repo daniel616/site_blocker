f02c74a5e74a622e1224d06c1b53991014f0e704
daniel <daniel.li@praetorian.com>
2021-04-26 22:59:21 -0500

    t

--- warnScript.js
+++ warnScript.js
@@ -1,4 +1,28 @@
-console.log(chrome.runtime.onMessage);
+
+
+let confirmString="I have taken time to think about what I am doing, and won't regret this later on";
+
+function randomizeString(st) {
+    let sArray=Array.from(st);
+    for (var i=0; i<3; i++){
+        let idx=Math.floor(Math.random()*st.length);
+        let digit= Math.floor(Math.random()*10);
+        sArray[idx]=digit;
+    }
+    return sArray.join('');
+}
+
+
+let stringToMatch=randomizeString(confirmString);
+
+var msgTxt = document.getElementById("messageText");
+msgTxt.innerText=stringToMatch;
+
+let confirmTxt=document.getElementById("confirmTxt");
+
+confirmTxt.setAttribute("pattern", stringToMatch);
+confirmTxt.setAttribute("onpaste", "return true;")
+
 
 chrome.runtime.onMessage.addListener(
     function(request,sender,sendResponse){
@@ -14,8 +38,10 @@ chrome.runtime.onMessage.addListener(
                 disableFor:duration,
                 restriction:request.reason.exp,
                 redirect:request.destUrl});
-            //location.href = request.destUrl;
+            location.href = request.destUrl;
         };
 
     }
 );
+
+console.log(chrome.runtime.onMessage);
